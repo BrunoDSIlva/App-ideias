6c8f922782d72268ff70bf388e68dcb8dae0b56e
Bruno Silva <Bruno178araujo@gmail.com>
2020-05-08 16:44:02 -0300

    novo codigo 08 deset

--- bin2de/controller.js
+++ bin2de/controller.js
@@ -1,11 +1,23 @@
 function convert () {
     let binar = window.document.getElementById('binary').value;
 
-    if (bianr >= 9 ){
+    if (binar.length >= 9 ){
         alert("You just can enter up 8 binary digits check you input field")
         return;
     }else if(binar.match(/^[0-1]/g) === null){
         alert("User can enter only 0 to 1")
         return;
+    }else {
+        
+        let result = 0;
+            for(let i = 0; i < binar.length; i++){
+                    let digit = binar.charAt(binar.length -i -1)
+                    result += Math.pow(2, i) * binar[binar.length - i - 1]
+                    
+        }
+        return document.getElementById('result') = result;
+        
     }
+    
+    
 }
