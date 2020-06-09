0ce41e47626674aa197bee00edfec1f93208628a
Bruno Silva <Bruno178araujo@gmail.com>
2020-06-08 22:30:09 -0300

    com ajuda fiz essa
    com ajuda eu fiz essa vou fazer uma nova amanha sem ajuda

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,37 +1,139 @@
     class Calculator {
-        constructor(previousdisplay, currentDisplay){
-            this.previousdisplay = previousdisplay;
-            this.currentDisplay =currentDisplay;
+        constructor(previousdisplayText, currentDisplayText){
+            this.previousdisplayText = previousdisplayText;
+            this.currentDisplayText = currentDisplayText;
+            this.clear();
         }
         clear(){ // it is going clear all the operation
+            this.currentOperand = '' ;
+            this.previousOperand = '';
+            this.operation = undefined;
+
 
         }
         delete(){ // it is going clear the last number
 
-        }
-        showDisplay(number){ // it show the display before add any mumber
+            this.currentOperand = this.currentOperand.toString().slice(0, -1)
 
         }
-        chooseOeration(operation){ // it chooses the operation of the calc
+        
+        hitNumber(number){ // it show the display before add any mumber
+
+            if(number === '.' && this.currentOperand.includes('.')) return
+            this.currentOperand = this.currentOperand.toString() + number.toString()
+
+        }
+        chooseOperation(operation){ // it chooses the operation of the calc
+            if(this.currentOperand === '') return
+            if(this.previousOperand !== ''){
+                this.computer()
+            }
+            this.operation = operation;
+            this.previousOperand = this.currentOperand
+            this.currentOperand = ''
 
         }
         
         computer(){ // it make the single number choosed became the number of calc 
 
+            let computation 
+            const prev = parseFloat(this.previousOperand)
+            const current = parseFloat(this.currentOperand)
+
+            if(isNaN(prev) || isNaN(current))return
+            switch(this.operation){
+                case '+':
+                    computation =prev + current
+                    break
+                case '-':
+                    computation =prev - current
+                    break
+                case '*':
+                    computation =prev * current
+                    break
+                case '/':
+                    computation =prev / current
+                    break
+                default:
+                    return
+            }
+            this.currentOperand = computation
+            this.operation = undefined
+            this.previousOperand = ''
+
+        }
+        getDisplayNumber(number){
+            const stringNumber = number.toString()
+            const integeDigit = parseFloat(stringNumber.split('.') [0])
+            const decimalDigit = stringNumber.split('.')[1]
+            let integerDisplay
+
+            if (isNaN(integeDigit)){
+                integerDisplay = ''
+            }else {
+                integerDisplay = integeDigit.toLocaleString('en', {
+                    maximumFractionDigits:0 })
+
+            }
+            if (decimalDigit != null){
+                return `${integerDisplay}.${decimalDigit}`
+            }else{
+                return integerDisplay
+            }
+
+            
         }
         updateDisplay(){ // this operation make the update of display when it is choosen the number or operation 
+            this.currentDisplayText.innerText = this.getDisplayNumber(this.currentOperand)
+
+            if (this.operation != null ){
+                this.previousdisplayText.innerText =
+               `${this.getDisplayNumber( this.currentOperand)
+               } ${this.operation}`
+
+            }
+            else{
+                this.previousdisplayText.innerText = ''
+            }
+
 
         }
     }
 
 
-    const numberButton = document.querySelectorAll('[data-number]');
-    const operationButton = document.querySelectorAll('[data-operation]');
-    const equalsButton = document.querySelector('[data-equals]');
-    const deleteButton = document.querySelector('[data-delete]');
-    const allclearButton = document.querySelector('[data-all-clear]');
-    const previousdisplay = document.querySelector('[data-previous-operation]');
-    const currentDisplay = document.querySelector('[data-current-operation]');
+        const numberButton = document.querySelectorAll('[data-number]');
+        const operationButton = document.querySelectorAll('[data-operation]');
+        const equalsButton = document.querySelector('[data-equals]');
+        const deleteButton = document.querySelector('[data-delete]');
+        const allClearButton = document.querySelector('[data-all-clear]');
+        const previousdisplayText = document.querySelector('[data-previous-operation]');
+        const currentDisplayText = document.querySelector('[data-current-operation]');
 
+        const calculator = new Calculator(previousdisplayText, currentDisplayText)
+        numberButton.forEach(button=> {
+            button.addEventListener('click', ()=>{
+                calculator.hitNumber(button.innerText)
+                calculator.updateDisplay()
+            })
+        })
+        operationButton.forEach(button=> {
+            button.addEventListener('click', ()=>{
+                calculator.chooseOperation(button.innerText)
+                calculator.updateDisplay()
+            })
+        })
 
+        equalsButton.addEventListener('click', button =>{
+            calculator.computer()
+            calculator.updateDisplay()
+        })
+
+        allClearButton.addEventListener('click', button =>{
+            calculator.clear()
+            calculator.updateDisplay()
+        })
+        deleteButton.addEventListener('click', button =>{
+            calculator.delete()
+            calculator.updateDisplay()
+        })
 
